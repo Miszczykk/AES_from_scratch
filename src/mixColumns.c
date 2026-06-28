9bcefeec05e988b3a2170c227e2b6d5019183179
miszc <miszczykmiloszk@gmail.com>
2026-06-28 14:07:03 +0200

    add inverseMixColumns function

--- src/mixColumns.c
+++ src/mixColumns.c
@@ -11,17 +11,30 @@ unsigned char state_matrix[SIZE_MATRIX][SIZE_MATRIX] = {
     {0x03, 0x01, 0x01, 0x02},
 };
 
+unsigned char inverse_state_matrix[SIZE_MATRIX][SIZE_MATRIX] = {
+    {0x0E, 0x0B, 0x0D, 0x09},
+    {0x09, 0x0E, 0x0B, 0x0D},
+    {0x0D, 0x09, 0x0E, 0x0B},
+    {0x0B, 0x0D, 0x09, 0x0E}
+};
+
 unsigned char galoisFieldMultiplication(unsigned char a, unsigned char b) {
-    switch (a) {
-        case 0x01:
-            return b;
-        case 0x02:
-            return b << 1 ^ (b & 0x80 ? 0x1B : 0x00);
-        case 0x03:
-            return b << 1 ^ (b & 0x80 ? 0x1B : 0x00) ^ b;
-        default:
-            return 0;
+    unsigned char result = 0;
+
+    for (int bit = 0; bit < 8; bit++) {
+        if ((b & 1) != 0)
+            result ^= a;
+
+        unsigned char high_bit_set = (a & 0x80);
+        a <<= 1;
+
+        if (high_bit_set != 0)
+            a ^= 0x1B;
+
+        b >>= 1;
     }
+
+    return result;
 }
 
 void mixColumns(unsigned char matrix[SIZE_MATRIX][SIZE_MATRIX]) {
@@ -34,6 +47,27 @@ void mixColumns(unsigned char matrix[SIZE_MATRIX][SIZE_MATRIX]) {
         for (int col = 0; col < SIZE_MATRIX; col++) {
             for (int index = 0; index < SIZE_MATRIX; index++) {
                 new_matrix[row][col] ^= galoisFieldMultiplication(state_matrix[row][index], matrix[index][col]);
+            }
+        }
+    }
+
+    for (int row = 0; row < SIZE_MATRIX; row++) {
+        for (int col = 0; col < SIZE_MATRIX; col++) {
+            matrix[row][col] = new_matrix[row][col];
+        }
+    }
+}
+
+void inverseMixColumns(unsigned char matrix[SIZE_MATRIX][SIZE_MATRIX]) {
+    if (matrix == NULL)
+        return;
+
+    unsigned char new_matrix[SIZE_MATRIX][SIZE_MATRIX] = {0};
+
+    for (int row = 0; row < SIZE_MATRIX; row++) {
+        for (int col = 0; col < SIZE_MATRIX; col++) {
+            for (int index = 0; index < SIZE_MATRIX; index++) {
+                new_matrix[row][col] ^= galoisFieldMultiplication(inverse_state_matrix[row][index], matrix[index][col]);
             }
         }
     }
